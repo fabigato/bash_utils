bcb5448100b6405ea9ac5e0850a4fa2d9602e018
fabigato <grsmasher@gmail.com>
2024-04-05 12:44:34 -0600

    added stdin input

--- scram.sh
+++ scram.sh
@@ -3,6 +3,21 @@ if [[ -z "${SCRAM_CHARS}" ]]; then
 	echo "You need to provide the scramble chars in the SCRAM_CHARS environment variable"
 	exit 1
 fi
-rev_chars="$(echo "$SCRAM_CHARS" | rev)"
-scrambled="$(echo "$1" | tr "$SCRAM_CHARS" "$rev_chars")"
-printf "$scrambled\n"
+
+function scramble_line {
+	rev_chars="$(echo "$SCRAM_CHARS" | rev)"
+	scrambled="$(echo "$1" | tr "$SCRAM_CHARS" "$rev_chars")"
+	printf "$scrambled\n"
+}
+
+if [ -p /dev/stdin ]; then
+    while IFS= read name; do
+	scramble_line "$name"
+    done
+else
+    if [ ! -z "$1" ]; then
+	scramble_line "$1"
+    else
+	echo "no input given"
+    fi
+fi
