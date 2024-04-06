9d3d7480048a8a2ab5d98eea4b5ba58c2a4ee219
fabigato <grsmasher@gmail.com>
2024-04-05 19:53:28 -0600

    swap_file.sh now reads file from argument and fallback to env var

--- swap_file.sh
+++ swap_file.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-file="/media/data_ext4/Dropbox/misc/misc/iubosu_gnd x.txt"
-# cat "$file" | ./scram.sh # > "$file"
-readarray -t lines < "$file"
+if [ ! -z "$1" ]; then
+	file="$1"
+else
+	if [ ! -z "$PORT_INDEX" ]; then
+		file="$PORT_INDEX"
+	else
+		echo either provide a file name or set the PORT_INDEX env variable
+		exit 1
+	fi
+fi
+readarray -t lines < "$file"  # read whole file in memory, otherwise you can´t update line by line while reading
 swapped_lines=()
 for line in "${lines[@]}"; do
     swapped_lines+=( "$(echo "$line" | ./scram.sh)" )
